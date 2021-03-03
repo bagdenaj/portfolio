d3ae5f5fa1ebe68eab3c287974995b6c7b7e1de7
bagdenaj <bagdenaj@8e86bfb0e784>
2021-03-03 19:51:23 +0000

    general overhaul

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import csv
-from flask import Flask, render_template, send_from_directory, request, redirect
+import time
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,14 +10,6 @@ def my_home():
 @app.route('/<path:subpath>')
 def show_subpath(subpath):
     return render_template(subpath)
-
-def write_to_file(data):
-	with open('database.txt', mode='a') as database:
-		name = data['name']
-		email = data['email']
-		phone = data['phone']
-		message = data['message']
-		file = database.write(f'\n{name},{email},{phone},{message}')
 
 def write_to_csv(data):
 	with open('database.csv', mode='a', newline='') as database2:
@@ -30,10 +23,14 @@ def write_to_csv(data):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-    	try:
-	    	data = request.form.to_dict()
-	    	write_to_csv(data)
-	    	return redirect('/thankyou.html')
-	    except:
-	    	return 'Did not save to database'
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database'
     return 'something went wrong'
+
+@app.route('/thankyou.html')
+def thankyou():
+    return render_template('thankyou.html'), {"Refresh": "5; url=https://bagdenaj.pythonanywhere.com"}
